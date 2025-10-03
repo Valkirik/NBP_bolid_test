918ffa1042ac11d9c929b13b4819e6ab277fb0c5
Valery <valkirik1995@mail.ru>
2025-10-03 15:23:33 +0300

    adding the tests

--- nbp_bolid/sensor_event/tests.py
+++ nbp_bolid/sensor_event/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from .models import Sensor, Event
 
-# Create your tests here.
+class EventsApiTests(APITestCase):
+        def setUp(self):
+            self.sensor_1 = Sensor.objects.create(name="Sensor 1", type=1)
+            self.sensor_2 = Sensor.objects.create(name="Sensor 2", type=2)
+            self.sensor_3 = Sensor.objects.create(name="Sensor 3", type=3)
+
+            Event.objects.create(sensor=self.sensor_3, temperature=3,)
+            Event.objects.create(sensor=self.sensor_2, temperature=5, humidity=3)
+            Event.objects.create(sensor=self.sensor_1, humidity=8)
+            Event.objects.create(sensor=self.sensor_1, temperature=3, humidity=1)
+            Event.objects.create(sensor=self.sensor_1, humidity=4)
+
+        def test_getting_all(self):
+            url = reverse("event-list")
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIsInstance(resp.data, dict)
+            self.assertEqual(len(resp.data["results"]), 5)
+
+        def test_certain_sensor_events(self):
+            url = reverse("sensor-events", kwargs={"pk": self.sensor_1.pk})
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIsInstance(resp.data, list)
+            self.assertEqual(len(resp.data), 3)
+
+
+        def test_getting_only_temperature(self):
+            url = reverse("events-only-temperature")
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+            objct = resp.data["results"][0]
+            self.assertIsNotNone(objct["temperature"])
+            self.assertIsNone(objct["humidity"])
+            result = resp.data["results"]
+            self.assertEqual(len(result), 1)
+
+        def test_getting_only_humidity(self):
+            url = reverse("events-only-humidity")
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+            objct = resp.data["results"][0]
+            self.assertIsNone(objct["temperature"])
+            self.assertIsNotNone(objct["humidity"])
+            result = resp.data["results"]
+            self.assertEqual(len(result), 2)
+
+        def test_getting_both(self):
+            url = reverse("both-humidity-temperature")
+            resp = self.client.get(url)
+            self.assertEqual(resp.status_code, 200)
+            objct = resp.data["results"][0]
+            self.assertIsNotNone(objct["temperature"])
+            self.assertIsNotNone(objct["humidity"])
+            result = resp.data["results"]
+            self.assertEqual(len(result), 2)
+
+
+
+
+
+
+
--- nbp_bolid/sensor_event/urls.py
+++ nbp_bolid/sensor_event/urls.py
@@ -5,12 +5,12 @@ from .endpoints import SensorViewSet, EventViewSet, TemperatureAndHumidityListAP
     TemperatureOnlyFilterListAPIView,HumidityOnlyFilterListAPIView
 
 router = routers.SimpleRouter()
-router.register("sensor_view", SensorViewSet),
+router.register("sensor_view", SensorViewSet, basename="sensor"),
 router.register("event_view", EventViewSet, basename="event"),
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("only_temperature/", TemperatureOnlyFilterListAPIView.as_view()),
-    path("only_humidity/", HumidityOnlyFilterListAPIView.as_view()),
-    path("temperature_and_humidity/", TemperatureAndHumidityListAPIView.as_view()),
+    path("only_temperature/", TemperatureOnlyFilterListAPIView.as_view(), name="events-only-temperature"),
+    path("only_humidity/", HumidityOnlyFilterListAPIView.as_view(), name="events-only-humidity"),
+    path("temperature_and_humidity/", TemperatureAndHumidityListAPIView.as_view(), name="both-humidity-temperature"),
 ]
